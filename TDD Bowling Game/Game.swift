93930bd933379101a7e8faa58eee51a8161624ef
Allen <allenwang@findata.com.tw>
2017-06-05 14:37:23 +0800

    first test - create Game, test gutter

---
+++ TDD Bowling Game/Game.swift
@@ -0,0 +1,19 @@
+//
+//  Game.swift
+//  TDD Bowling Game
+//
+//  Created by 王適緣 on 2017/6/5.
+//  Copyright © 2017年 王適緣. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    func roll(pins: Int) {
+        
+    }
+    
+    func score() -> Int {
+        return 0
+    }
+}
