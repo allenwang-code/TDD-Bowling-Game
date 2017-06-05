b11372e61908ab12d1233e16afc87f7bf6ee64e7
Allen <allenwang@findata.com.tw>
2017-06-05 17:44:10 +0800

    third test- spare pass

--- TDD Bowling Game/Game.swift
+++ TDD Bowling Game/Game.swift
@@ -9,13 +9,35 @@
 import Foundation
 
 class Game {
-    var score = 0
+    var currentRoll = 0
+    
+    var rolls = [Int]()
     
     func roll(pins: Int) {
-        score += pins
+        currentRoll += 1
+    
+        rolls.append(pins)
     }
     
     func getScore() -> Int {
+        let noTenthBonus = rolls.count == 20
+        if noTenthBonus{ rolls.append(0) }
+        
+        var score = 0
+        
+        for i in 0...19 {
+            score += rolls[i]
+        }
+        
+        for i in 0...18 {
+            let odd = rolls[i]
+            let even = rolls[i+1]
+            let bonus = rolls[i+2]
+            if odd + even == 10 {
+                score += bonus
+            }
+        }
+        
         return score
     }
 }
